23586f3082be441adc2e79dd0a574c09af093f65
spomni <mikhail.snitko@gmail.com>
2019-09-23 19:19:19 +0300

    feat: Find solution.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,61 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports = function check(str, config) {
+
+  let stack = [];
+  
+  let actions = config.reduce((actions, pair) => {
+
+    let [opening, closing] = pair;
+
+    if (opening === closing){
+
+      actions[opening] = () => {
+        
+        if (stack.length === 0){
+          stack.push(opening);
+          
+        } else {
+          let last = stack.pop();
+          
+          if (last !== opening){
+            stack.push(last);
+            stack.push(opening);
+          }
+        }
+      }
+      
+    } else if (opening !== closing){
+    
+      actions[opening] = () => {
+        stack.push(opening);
+      }
+      
+      actions[closing] = () => {
+        if ( stack.length === 0
+          || stack.pop() !== opening
+        ){
+          throw new Error('last !== opening');
+        }
+      }
+    }
+    
+    return actions;
+  }, {});
+
+  try {
+    
+    str
+      .split('')
+      .forEach((symbol) => {
+        actions[symbol]();
+      });
+      
+    if (stack.length !== 0){
+      throw new Error('length !== 0')
+    }
+    
+  } catch (e){
+    return false;
+  }
+  
+  return true;
 }
